4a17570fe847f30d010997615afbecf012347ba5
karan <yadavkaran471@gmail.com>
2023-08-06 18:57:51 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,14 +1,43 @@
-
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/Pizza-db');
+var mongoURL =
+  "mongodb+srv://yadavkaran471:UodgikBSn4hL8ojT@cluster0.67r13yb.mongodb.net/pizza-db";
 
-const db = mongoose.connection;
+mongoose.connect(mongoURL);
 
-db.on("error", console.error.bind(console, "error in connecting to database"));
+var db = mongoose.connection;
 
-db.once("open", function () {
-  console.log("successfully connected to database");
+db.on("connected", () => {
+  console.log("Mongo db is successfully connected");
+});
+db.on("error", () => {
+  console.log("Mongo db connection failed");
 });
 
-module.exports = db;
+module.exports = mongoose;
+
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://yadavkaran471:QzPOHviCvK0yxib4@cluster0.67r13yb.mongodb.net/pizza-db"
+//     );
+//     console.log("connect to mongoDB successfully");
+//   } catch (error) {
+//     console.log("connnection failed", error);
+//   }
+// };
+
+// // mongoose.connect(process.env.MONGO_CONNECTION_URL);
+
+// // const db = mongoose.connection;
+
+// // db.on("error", console.error.bind(console, "error in connecting to database"));
+
+// // db.once("open", function () {
+// //   console.log("successfully connected to database");
+// // });
+
+// module.exports = connectDB;
